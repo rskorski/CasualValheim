dfaf0e05415971a49b04c399aaea6c49e49c1e7b
Rich Skorski <26683026+rskorski@users.noreply.github.com>
2025-04-06 19:33:55 -0400

    sneaking, fishing and bow draw considered for stamina drain
    * all three hooked up with config flags.
    * flipped the order of the tests in UseStamina, allowing the flag check to short circuit the state check.  As is, this feels like a micro optimization, but a first run at trying to detect fishing has a lot more logic that made this order feel better so I kept it.

--- OdinsEnudrance.cs
+++ OdinsEnudrance.cs
@@ -20,6 +20,9 @@ namespace CasualValheim
         private static ConfigEntry<bool> InfStamina;
         private static ConfigEntry<bool> InfStaminaTargeted;
         private static ConfigEntry<bool> InfStaminaSwimming;
+        private static ConfigEntry<bool> InfStaminaBow;
+        private static ConfigEntry<bool> InfStaminaFishing;
+        private static ConfigEntry<bool> InfStaminaSneaking;
 
         partial void OdinsEnduranceConfig()
         {
@@ -28,6 +31,9 @@ namespace CasualValheim
             InfStamina = Config.Bind("OdinsEndurance", "InfStamina", true, "Prevent stamina drain, considering the state specific flags.");
             InfStaminaTargeted = Config.Bind("OdinsEndurance", "InfStaminaTargeted", false, "Prevent stamina drain while targeted.");
             InfStaminaSwimming = Config.Bind("OdinsEndurance", "InfStaminaSwimming", false, "Prevent stamina drain while swimming.");
+            InfStaminaBow = Config.Bind("OdinsEndurance", "InfStaminaBow", false, "Prevent stamina drain while drawing a bow, the distance can keep you out of detection range.");
+            InfStaminaFishing = Config.Bind("OdinsEndurance", "InfStaminaFishing", false, "Prevent stamina drain while casting and reeling a rod.");
+            InfStaminaSneaking = Config.Bind("OdinsEndurance", "InfStaminaSneaking", false, "Prevent stamina drain while sneaking.");
         }
 
         ////////////////////////////////////////
@@ -54,18 +60,44 @@ namespace CasualValheim
         {
             static bool Prefix(Player __instance, float v)
             {
-                if ( !InfStamina.Value )
+                if( !InfStamina.Value )
                 {
                     return true;
                 }
-                if( __instance.IsSwimming() && !InfStaminaSwimming.Value )
+                if( !InfStaminaSwimming.Value && __instance.IsSwimming() )
                 {
                     return true;
                 }
-                if( __instance.IsTargeted() && !InfStaminaTargeted.Value )
+                if( !InfStaminaTargeted.Value && __instance.IsTargeted() )
                 {
                     return true;
                 }
+                if( !InfStaminaSneaking.Value && __instance.IsCrouching() )
+                {
+                    return true;
+                }
+
+                // bow draw and fishing look to the weapon in hand
+                ItemDrop.ItemData weapon = __instance.GetCurrentWeapon();
+                if( null != weapon )
+                {
+                    if( !InfStaminaBow.Value &&
+                        weapon.m_shared.m_animationState == ItemDrop.ItemData.AnimationState.Bow &&
+                        __instance.IsDrawingBow() )
+                    {
+                        return true;
+                    }
+
+                    bool isCasting = __instance.IsDrawingBow();
+                    bool isReeling = __instance.IsBlocking();
+                    if ( !InfStaminaFishing.Value &&
+                         weapon.m_shared.m_animationState == ItemDrop.ItemData.AnimationState.FishingRod &&
+                         (isCasting || isReeling) )
+                    {
+                        return true;
+                    }
+                }
+
                 return false;
             }
         }
